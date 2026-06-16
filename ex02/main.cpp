a90f27027571be25e6a6b528513f3df03837a584
WSL HOME <raulrecrec@gmail.com>
2026-06-16 23:44:35 +0200

    Add Fixed operator tests
    - Created main.cpp to test increment and arithmetic operators.
    - Added tests for fixed-point multiplication and max comparison.
    - Verified output against the subject example.

---
+++ ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 15:23:23 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/16 23:42:57 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed		a;
+	Fixed const	b(Fixed(5.05f) * Fixed(2));
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
+
+	return (0);
+}
