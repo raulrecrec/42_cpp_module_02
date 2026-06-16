f0806ad84c1f7f1fbe65a018c58fb8feb6d19a8c
WSL HOME <raulrecrec@gmail.com>
2026-06-16 16:55:04 +0200

    Add Fixed conversion tests
    - Created main.cpp to test integer and float Fixed constructors.
    - Printed fixed-point values using the output stream operator.
    - Added tests for conversion back to integer values.

---
+++ ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 15:23:23 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/16 16:51:39 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main( void ) 
+{
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+
+	a = Fixed( 1234.4321f );
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	
+	return (0);
+}
