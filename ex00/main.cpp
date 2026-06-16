3da403fa8e517fde0c579863a8462afce9b093f9
WSL HOME <raulrecrec@gmail.com>
2026-06-16 16:36:32 +0200

    Add Fixed class tests
    - Created main.cpp to test Fixed object construction and copy behavior.
    - Added tests for copy constructor and assignment operator.
    - Printed raw fixed-point values using getRawBits.

---
+++ ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 15:23:23 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/16 16:35:57 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed	a;
+	Fixed	b;
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << "\n";
+	std::cout << b.getRawBits() << "\n";
+	std::cout << c.getRawBits() << "\n";
+
+	return (0);
+}
